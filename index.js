d85f301ca130ae019088bb06ae87355c7e9ac4f2
arjuntudu <arjuntudu9163@gmail.com>
2024-12-02 12:56:50 +0530

    awdawdawdwd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:3000"]
+    origin:["http://localhost:3000","https://react2-frontend.vercel.app"]
 }))
 
 
